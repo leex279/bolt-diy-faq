a42e23a1954aed79e7a7ae12926ecc39a9e3f9c2
Leex <thomas@thirty3.de>
2025-01-11 15:48:39 +0100

    Initial commit from your app

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 
     // https://vitejs.dev/config/
     export default defineConfig({
-      plugins: [react()],
+      plugins: [react({
+        babel: {
+          presets: ["@babel/preset-react"]
+        }
+      })],
       base: '/bolt-diy-faq/',
     });
